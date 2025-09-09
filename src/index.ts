854cead3c1cab3b5faf4a157e89aa3bb825b458e
Pedro Moitinho <pedrohenriquemoitinho.prado@ucsal.edu.br>
2025-09-08 22:08:28 -0300

    additions

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Swagger configuration
 const swaggerOptions = {
